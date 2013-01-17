0014fbc2788634ed594d8bd55a99fe1e931c5350
Den <denisranneft@gmail.com>
2013-01-17 18:38:36 +0400

    up2

--- gen/ru/simpra/sms_cleaner/R.java
+++ gen/ru/simpra/sms_cleaner/R.java
@@ -15,7 +15,8 @@ public final class R {
     }
     public static final class id {
         public static final int list=0x7f070000;
-        public static final int menu_delete=0x7f070001;
+        public static final int listHeader=0x7f070001;
+        public static final int menu_delete=0x7f070002;
     }
     public static final class layout {
         public static final int activity_sms_cleaner=0x7f030000;
@@ -54,5 +55,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int Heading=0x7f050002;
     }
 }
--- src/ru/simpra/sms_cleaner/SmsCleaner.java
+++ src/ru/simpra/sms_cleaner/SmsCleaner.java
@@ -6,9 +6,12 @@ import java.util.regex.Pattern;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.BaseColumns;
 import android.provider.ContactsContract;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
@@ -16,10 +19,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class SmsCleaner extends Activity {
 
-	private String[] regexp = {
+	final private String[] regexp = {
 			"Услуга.+Кто Звонил?.+этот абонент звонил Вам \\d+ раз",
 			"Этот абонент снова в сети",
 			"Ваш запрос достален абоненту \\+?\\d+. Осталось запросов: \\d+",
@@ -28,103 +32,154 @@ public class SmsCleaner extends Activity {
 			"Этот абонент звонил.+\\d+ раз",
 			"Этот абонент оставил Вам \\d+ нов(ое|ых) голосов(ое|ых) сообщени(я|е|й)",
 			"Абонент \\+?\\d+ просит Вас.+перезвонить.+",
-			"Абонент \\+?\\d+ не смог Вам дозвониться"
-			};
-	private ArrayList<String> idList;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sms_cleaner);
-        
-        ArrayList<ArrayList> List = fetchSMSList();
-        ArrayList<String> smsList = List.get(0);
-        ListView lViewSMS = (ListView) findViewById(R.id.list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsList);
-        lViewSMS.setAdapter(adapter);
-        this.idList = List.get(1);
-    }
+			"Абонент \\+?\\d+ не смог Вам дозвониться" };
+	private ArrayList<String> idList = new ArrayList<String>();
+	private ArrayList<String> smsList = new ArrayList<String>();
+	private int smsQ = 0;
+	private ListView lViewSmS;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_sms_cleaner, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-    	   switch (item.getItemId()) {
-    	    case R.id.menu_delete:
-    	        DeleteFound();
-    	        return true;
-    	    default:
-    	        return super.onOptionsItemSelected(item);
-    	    }
-    }
-    
-    public ArrayList<ArrayList> fetchSMSList()
-    {
-    	ArrayList<String> smsList = new ArrayList<String>();
-    	ArrayList<String> idList = new  ArrayList<String>();
-    	ArrayList<ArrayList> List = new  ArrayList<ArrayList>();
-    	Uri uriSms = Uri.parse("content://sms/inbox");
-        Cursor cursor = getContentResolver().query(uriSms, new String[]{"_id", "address", "date", "body"},null,null,null);
-        cursor.moveToFirst();
-        while  (cursor.moveToNext())
-        {
-        	   String address = cursor.getString(1);
-               String body = cursor.getString(3);
-               String _id = cursor.getString(0);
-               for(String s:this.regexp)
-               {
-	               Pattern p = Pattern.compile(s);
-	               Matcher m = p.matcher(body);
-	               if (m.find()) {
-	            	   String name =  getContactDisplayNameByNumber(address);
-	            	   smsList.add(name+"\nПолучено: "+body.substring(0, 30)+"...");
-	            	   idList.add(_id);
-	               }
-               }
-        }
-        cursor.close();
-        List.add(smsList);
-        List.add(idList);
-    	
-    	return List;
-    }
-    
-    public String getContactDisplayNameByNumber(String number) {
-        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
-        String name = "?";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sms_cleaner);
+		this.lViewSmS = (ListView) findViewById(R.id.list);
+		fetchSMSList();
+		populateSmSList(this.smsList);
+	}
 
-        ContentResolver contentResolver = getContentResolver();
-        Cursor contactLookup = contentResolver.query(uri, new String[] {BaseColumns._ID,
-                ContactsContract.PhoneLookup.DISPLAY_NAME }, null, null, null);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_sms_cleaner, menu);
+		return true;
+	}
 
-        try {
-            if (contactLookup != null && contactLookup.getCount() > 0) {
-                contactLookup.moveToNext();
-                name = contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
-                //String contactId = contactLookup.getString(contactLookup.getColumnIndex(BaseColumns._ID));
+	private void populateSmSList(ArrayList<String> smsList) {
+		if (smsList.size() > 0) {
+			TextView listHeader = (TextView) findViewById(R.id.listHeader);
+			listHeader.append("Найдено: " + this.idList.size());
+		} else {
+
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, smsList);
+		this.lViewSmS.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		switch (item.getItemId()) {
+		case R.id.menu_delete:
+			deleteFound();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private ProgressDialog startDialog(String msg, int maxVal) {
+		ProgressDialog progressDialog = new ProgressDialog(this);
+		progressDialog.setMessage(msg);
+		progressDialog.setCancelable(false);
+		progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		progressDialog.setProgress(0); // set percentage completed to 0%
+		progressDialog.setMax(maxVal);
+		progressDialog.show();
+		return progressDialog;
+	}
+
+	public void fetchSMSList() {
+
+		
+		
+		Uri uriSms = Uri.parse("content://sms/inbox");
+		final Cursor smsLookup = getContentResolver().query(uriSms,
+				new String[] { "_id", "address", "date", "body" }, null, null,
+				null);
+		this.smsQ = smsLookup.getCount();
+		final ProgressDialog pd = startDialog("Мы начинаем КВН", this.smsQ);
+		final Handler responseHandler = new Handler() 
+        {                               
+            public void handleMessage(Message msg)  
+            {
+                super.handleMessage(msg);
+                try 
+                {
+                    pd.dismiss();
+                } 
+                catch (Exception e) 
+                {
+                    e.printStackTrace();
+                }                       
             }
-        } finally {
-            if (contactLookup != null) {
-                contactLookup.close();
+        };
+		
+		Thread thread = new Thread(new Runnable() {    
+            @Override
+            public void run() {
+                //downloading code
+                responseHandler.sendEmptyMessage(0);
             }
-        }
+         });
+         thread.start();
+		
+		smsLookup.moveToFirst();
+		int i = 1;
+		while (smsLookup.moveToNext()) {
+			i++;
+			String address = smsLookup.getString(1);
+			String body = smsLookup.getString(3);
+			String _id = smsLookup.getString(0);
+			for (String s : regexp) {
+				Pattern p = Pattern.compile(s);
+				Matcher m = p.matcher(body);
+				if (m.find()) {
+					String name = getContactDisplayNameByNumber(address);
+					smsList.add(name + "\nПолучено: " + body.substring(0, 30)
+							+ "...");
+					idList.add(_id);
+				}
+			}
+		}
+		smsLookup.close();
+	}
 
-        return name;
-    }
-    
-    public void DeleteFound()
-    {
-    	for(int i = 0; i < this.idList.size(); i++) {
-    		getContentResolver().delete(Uri.parse("content://sms/" + idList.get(i)),null,null);
-        }
-    	ListView lViewSMS = (ListView) findViewById(R.id.list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
-        lViewSMS.setAdapter(adapter);
-    }
-    
+	public String getContactDisplayNameByNumber(String number) {
+		Uri uri = Uri.withAppendedPath(
+				ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+				Uri.encode(number));
+		String name = "?";
+
+		ContentResolver contentResolver = getContentResolver();
+		Cursor contactLookup = contentResolver.query(uri, new String[] {
+				BaseColumns._ID, ContactsContract.PhoneLookup.DISPLAY_NAME },
+				null, null, null);
+
+		try {
+			if (contactLookup != null && contactLookup.getCount() > 0) {
+				contactLookup.moveToNext();
+				name = contactLookup.getString(contactLookup
+						.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+				// String contactId =
+				// contactLookup.getString(contactLookup.getColumnIndex(BaseColumns._ID));
+			}
+		} finally {
+			if (contactLookup != null) {
+				contactLookup.close();
+			}
+		}
+
+		return name;
+	}
+
+	public void deleteFound() {
+		
+		for (int i = 0; i < this.idList.size(); i++) {
+			// getContentResolver().delete(Uri.parse("content://sms/" +
+			// idList.get(i)),null,null);
+		}
+		populateSmSList(new ArrayList<String>());
+	}
+
 }
