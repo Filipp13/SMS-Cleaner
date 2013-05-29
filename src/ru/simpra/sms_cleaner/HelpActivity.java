facd632534ccdc566e624c3bfb09523820bc5c54
Den <denisranneft@gmail.com>
2013-05-29 12:57:44 +0400

    version 1.2.0

---
+++ src/ru/simpra/sms_cleaner/HelpActivity.java
@@ -0,0 +1,20 @@
+package ru.simpra.sms_cleaner;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.os.Build;
+
+public class HelpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+		// Show the Up button in the action bar.
+		
+	}
+}
